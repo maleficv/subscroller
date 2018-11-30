1b783656f347ddf52c4ac9111f35fcf305087e63
Adrian Piwowarczyk <adrpiw@gmail.com>
2018-11-30 23:20:57 +0000

    [Project] Resolving js cache with hash in filename

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -7,7 +7,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, '../dist'),
         publicPath: '/',
-        filename: 'bundle.js'
+        filename: '[name].[contenthash].js'
     },
     module: {
         rules: [
