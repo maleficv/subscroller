7be9d6580fca61d3ecfdd528b2c36defaf6bb2cb
Adrian Piwowarczyk <adrpiw@gmail.com>
2018-12-01 00:38:23 +0000

    [App] Reordering styles

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {Router} from '@reach/router';
 import {injectGlobal} from 'react-emotion';
-import 'normalize.css';
 
 import Navigation from './components/Navigation';
 import Home from './components/Home';
