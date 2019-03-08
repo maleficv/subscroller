6dc342330731492e12d9652c3623755778eb2155
Adrian Piwowarczyk <adrpiw@gmail.com>
2019-03-08 22:16:37 +0000

    [Proxy] Added newline to server logs

--- src/proxy/actions/logServerEvent.js
+++ src/proxy/actions/logServerEvent.js
@@ -1,3 +1,3 @@
 module.exports = function logServerEvent(port, date, callback) {
-    callback(`${date} server started on ${port}`, 'server');
+    callback(`${date} server started on ${port}\n`, 'server');
 };
