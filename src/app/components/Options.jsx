9dd44aedbba61c002f93c4d57012b0e2baf69b20
Adrian Piwowarczyk <adrpiw@gmail.com>
2020-04-10 04:48:01 +0100

    [App] Styled buttons and new subreddits

--- src/app/components/Options.jsx
+++ src/app/components/Options.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import colors from '../theme';
+
+const Button = styled('button')`
+    padding: 12px;
+    color: white;
+    background-color: ${colors.primary};
+    border: 0;
+    border-left: 0px;
+    margin-right: 4px;
+
+    &::-moz-focus-inner {
+    border: 0;
+    }
+`;
 
 const Options = ({updateOptions}) => (
     <div>
         Sort by:
         <br />
-        <button onClick={() => updateOptions({sortBy: 'new'})}>New</button>
-        <button onClick={() => updateOptions({sortBy: 'top'})}>Top</button>
+        <Button onClick={() => updateOptions({sortBy: 'new'})}>New</Button>
+        <Button onClick={() => updateOptions({sortBy: 'top'})}>Top</Button>
     </div>
 );
 
--- src/config/subreddits.js
+++ src/config/subreddits.js
@@ -1,15 +1,15 @@
 const subreddits = [
     {
-        image: 'https://external-preview.redd.it/vwebCEc9meCPtx9_U38WEyZ0mB9Z4uSOuHzpwqX8NO4.jpg?width=640&crop=smart&auto=webp&s=a6595c10972bfe573ddfb6c688c5802aadcc904b',
-        sub: 'EarthPorn',
+        image: 'https://preview.redd.it/ae9nplvv22h41.jpg?width=960&crop=smart&auto=webp&s=d5ab0bbd0c340b3325a6a540d84ba66d90bf4c22',
+        sub: 'LondonPics',
     },
     {
         image: 'https://external-preview.redd.it/enRnUx3a5IKbbBpoV0sABvGFnt_32GRakkSB8domqJs.jpg?width=640&crop=smart&auto=webp&s=17115bdc7aff1a207312a06668dada8663bdbfdc',
         sub: 'food',
     },
     {
-        image: 'https://preview.redd.it/28bhzzz9tjf11.jpg?width=640&crop=smart&auto=webp&s=ada23efd59693aa341229c67c8f01f40f1f2019a',
-        sub: 'NatureIsFuckingLit',
+        image: 'https://i.redd.it/ttrxbu2bovr41.jpg',
+        sub: 'catpictures',
     },
     {
         image: 'https://preview.redd.it/gk32coo1io511.jpg?width=640&crop=smart&auto=webp&s=c95cbac1e2607b2c35620ab3af95f2d43e9d3fe3',
@@ -24,8 +24,8 @@ const subreddits = [
         sub: 'PixelArt',
     },
     {
-        image: 'https://external-preview.redd.it/Y0bLApopz1vKjRWEUpe_WaCjfEBdk75DLP0_lA4YoIw.jpg?width=640&crop=smart&auto=webp&s=67f6e68531ccb12e6c7d3aa8ed02356317cba440',
-        sub: 'CityPorn',
+        image: 'https://i.redd.it/quaot4zw6ur41.jpg',
+        sub: 'OldSchoolCool',
     },
     {
         image: 'https://external-preview.redd.it/fNFaimFZAnwsT2TI0oz7vaG59wrx0bEBspnJO9ma4VU.jpg?width=640&crop=smart&auto=webp&s=31906f1f18f6a06961c5521bd3d3a69f89343f6a',
